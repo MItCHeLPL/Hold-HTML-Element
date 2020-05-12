67597511b81ecfb820d90551173d1b44ceafe37f
MItCHeLPL <45109417+MItCHeLPL@users.noreply.github.com>
2020-05-12 15:35:46 +0200

    improved script and deleted cursor loading bar

--- Hold.js
+++ Hold.js
@@ -1,156 +1,121 @@
-//SETTINGS
-var loadingBar = document.getElementById("loadingBar"); //specify id of element that you want to use as loading bar
-var loadingBarUpdateRate = 10; //How often update loading bar in miliseconds (min 5)
+//Feel free to modify these functions to your needs 
 
+//When holding started
+function holdStart(mainListener, time)
+{
+    mainListener.style.cursor = "wait";
 
+    document.documentElement.style.setProperty("--holdTime", time+"s");
 
-/**
- * When held for set amount of time script executes this function, feel free to modify it.
- */
-function afterLongpress(mainListener)
+    mainListener.classList.add("loadingAnimationClass");
+
+    console.log("Started holding");
+}
+
+//While holding
+function duringHold(mainListener, time)
+{
+    console.log(Math.ceil(loadingProgress)+"%");
+}
+
+//When mouse button is up before time passed
+function canceledHold(mainListener, time)
+{
+    mainListener.style.cursor = "auto";
+
+    mainListener.classList.remove("loadingAnimationClass");
+
+    console.log("Canceled holding");
+}
+
+//When succesfuly held for set amount of time
+function afterHold(mainListener, time)
 {
     mainListener.className = "green";
     mainListener.firstElementChild.innerHTML = "Success!";
+
+    mainListener.classList.remove("loadingAnimationClass");
+
+    mainListener.style.cursor = "auto";
+
+    console.log("Finished holding");
 }
 
 
 
 
 //SCRIPT
-var mainListeners = document.querySelectorAll('[data-holdMode="standard"]'); //elements with hold enabled  
-var mainListenersBar = document.querySelectorAll('[data-holdMode="bar"]'); //elements with hold enabled and loading bar enabled
+var mainListeners = document.querySelectorAll('[data-hold]'); //elements with hold enabled  
 
-//without bar
 mainListeners.forEach(function(value, index){
-    mainListeners[index].addEventListener("mousedown", function(){longPress(mainListeners[index], parseInt(mainListeners[index].getAttribute("data-holdTime")))});
-    mainListeners[index].addEventListener("touchstart", function(){longPress(mainListeners[index], parseInt(mainListeners[index].getAttribute("data-holdTime")), false, true)}, {passive: true}); //don't use loading bar when using mobile
+    mainListeners[index].addEventListener("mousedown", function(){longPress(mainListeners[index], parseInt(mainListeners[index].getAttribute("data-hold")))});
+    mainListeners[index].addEventListener("touchstart", function(){longPress(mainListeners[index], parseInt(mainListeners[index].getAttribute("data-hold")), true)}, {passive: true}); //mobile
 });
 
-//when using bar
-mainListenersBar.forEach(function(value, index){
-    mainListenersBar[index].addEventListener("mousedown", function(){longPress(mainListenersBar[index], parseInt(mainListenersBar[index].getAttribute("data-holdTime")), true)});
-    mainListenersBar[index].addEventListener("touchstart", function(){longPress(mainListenersBar[index], parseInt(mainListenersBar[index].getAttribute("data-holdTime")), true, true)}, {passive: true}); //don't use loading bar when using mobile
-});
-
-var loadingBarProgress = 0; //From 0 to 100
+var loadingProgress = 0; //From 0 to 100
+var loadingUpdateRate = 10; //(min 5) how often check progress in ms
 
 /**
  * Main fuction for initializng intervals ant detecting hold
  * @param {*} mainListener HTML Element that is being held
  * @param {*} time How much time to hold element
- * @param {*} loadingBarEnabled Do you want to use loading bar
  * @param {*} mobile Does call come from mobile device
  */
-function longPress(mainListener, time, loadingBarEnabled, mobile)
+function longPress(mainListener, time, mobile)
 {  
-    if((typeof mobile !== "undefined" && mobile == true) || typeof loadingBarEnabled === "undefined" || loadingBarEnabled == false) //Disable loading bar
-    {
-        loadingBarEnabled = false;
-    }
+    //Initialize loading progress update
+    var loadingUpdateInterval = setInterval(function(){
+        calculateLoading(mainListener, time);
+    }, loadingUpdateRate);
 
-    mainListener.style.cursor = "wait";
-
-    if(loadingBarEnabled) 
-    {
-        //Loading bar position default position
-        var x = 0;
-        var y = 0;
-
-        //Initialize loading bar progress update
-        var loadingBarUpdateInterval = setInterval(function(){
-            calculateLoadingBar(time);
-        }, loadingBarUpdateRate);
-
-        //Set loading bar position to the cursor position
-        mainListener.addEventListener("mousemove", function(e){
-            x = e.pageX;
-            y = e.pageY;
-            loadingBarPositon(x, y);
-        });
-
-        document.documentElement.style.setProperty("--loadingBarTime", time+"s");
-
-        loadingBar.classList.add("lbAnimationClass");
-
-        //Display loading bar
-        loadingBar.style.display="block";
-    }
+    holdStart(mainListener, time);//execute at the start of hold
 
     //Base hold interval
     var interval = setInterval(function(){
 
 
 
-        afterLongpress(mainListener);//Execute this after to do after longpress succeeded
+        afterHold(mainListener, time);//Execute this after to do after hold succeeded
         
 
 
-        clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval);
+        clearIntervals(mainListener, interval, loadingUpdateInterval);
     }, time * 1000);
 
     //Cancel longpress
-    mainListener.addEventListener("mouseup", function(){clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval)}); 
-    mainListener.addEventListener("mouseleave", function(){clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval)}); 
+    mainListener.addEventListener("mouseup", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)}); 
+    mainListener.addEventListener("mouseleave", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)}); 
     if(mobile)
     {
         mainListener.addEventListener('contextmenu', e => {
             e.preventDefault();
           });
         
-        mainListener.addEventListener("touchcancel", function(){clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval)});
-        mainListener.addEventListener("touchend", function(){clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval)});
+        mainListener.addEventListener("touchcancel", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)});
+        mainListener.addEventListener("touchend", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)});
     }
 }
 
-function calculateLoadingBar(time)
+function calculateLoading(mainListener, time)
 {
     //Calculating interval progress from 0 to 100
-    var unit = (loadingBarUpdateRate / time)/10;
-    loadingBarProgress += unit;
+    var unit = (loadingUpdateRate / time)/10;
+    loadingProgress += unit;
 
-    //You can manipulate loading bar here
-
-    /*document.documentElement.style.setProperty("--loadingBarProgress", 1-(loadingBarProgress/100)+"em");*/
-
-}
-
-//Set position of loading bar
-function loadingBarPositon(x, y)
-{
-
-
-    //You can change offsets to whatever you like
-    var offsetX = 7.5;
-    var offsetY = 7.5;
-
-
-
-    //Set loading bar position
-    loadingBar.style.top = y + offsetY +"px";
-    loadingBar.style.left = x + offsetX +"px";
-}
-
-function clearLoadingBar()
-{
-    loadingBarProgress = 0; //reset progress bar
-    loadingBar.style.display = "none"; //hide loading bar
-    loadingBar.classList.remove("lbAnimationClass");
+    duringHold(mainListener, time); //Execute this during holding
 }
 
 //Clear intervals and listeners
-function clearIntervals(mainListener, interval, loadingBarEnabled, loadingBarUpdateInterval)
+function clearIntervals(mainListener, interval, loadingUpdateInterval)
 {
-    mainListener.style.cursor = "auto";
+    clearInterval(loadingUpdateInterval);
+    
+    loadingProgress = 0; //reset progress
 
-    if(loadingBarEnabled)
-    {
-        clearLoadingBar();
-        clearInterval(loadingBarUpdateInterval);
-    }
     clearInterval(interval);
 
-    mainListener.removeEventListener("mouseup", function(){clearIntervals(interval, loadingBarUpdateInterval)}); 
-    mainListener.removeEventListener("mouseleave", function(){clearIntervals(interval, loadingBarUpdateInterval)}); 
-    mainListener.removeEventListener("touchcancel", function(){clearIntervals(interval, loadingBarUpdateInterval)});
-    mainListener.removeEventListener("touchend", function(){clearIntervals(interval, loadingBarUpdateInterval)});
+    mainListener.removeEventListener("mouseup", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)}); 
+    mainListener.removeEventListener("mouseleave", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)}); 
+    mainListener.removeEventListener("touchcancel", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)});
+    mainListener.removeEventListener("touchend", function(){clearIntervals(mainListener, interval, loadingUpdateInterval); canceledHold(mainListener, time)});
 }
--- README.md
+++ README.md
@@ -3,12 +3,11 @@ Hold HTML Element for set amount of time to execute function.<br />
 <br />
 Just download Hold.js script from:<br />
 https://raw.githubusercontent.com/MItCHeLPL/Hold-HTML-Element/master/Hold.js<br />
-and import it in <body> with:<br />
+and import it in the last line of <body> with:<br />
 <script src="Hold.js"></script><br />
 <br />
-And add these attributes to any element on your page:<br />
-data-hold="standard" or data-hold="bar",<br />
-data-time="[how many seconds you want to wait]"<br />
+Edit HOld.js top fucntions to your needs and add these attributes to any element on your page:<br />
+data-hold="[how many seconds you want to wait]"<br />
 <br />
 Example:
 https://mitchelpl.github.io/Hold-HTML-Element/
